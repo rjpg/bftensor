349f0c3aad5e2768e94f4f7b741f5f7ff4328d64
rjpg <rjpg@fe.up.pt>
2017-12-19 14:25:25 +0000

    autoencoder for classification file added

---
+++ Autoencoder/src/AutoEncoderMNIST.py
@@ -0,0 +1,74 @@
+'''
+Created on 19/12/2017
+
+@author: rjpg
+'''
+
+
+from keras.datasets import mnist 
+from keras.models import Model 
+from keras.layers import Input, Dense 
+from keras.utils import np_utils 
+import numpy as np
+
+num_train = 60000
+num_test = 10000
+
+height, width, depth = 28, 28, 1 # MNIST images are 28x28
+num_classes = 10 # there are 10 classes (1 per digit)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(num_train, height * width)
+X_test = X_test.reshape(num_test, height * width)
+X_train = X_train.astype('float32') 
+X_test = X_test.astype('float32')
+
+X_train /= 255 # Normalise data to [0, 1] range
+X_test /= 255 # Normalise data to [0, 1] range
+
+Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
+Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+
+input_img = Input(shape=(height * width,))
+
+x = Dense(height * width, activation='relu')(input_img)
+
+encoded = Dense(height * width//2, activation='relu')(x)
+encoded = Dense(height * width//8, activation='relu')(encoded)
+
+y = Dense(height * width//256, activation='relu')(x)
+
+decoded = Dense(height * width//8, activation='relu')(y)
+decoded = Dense(height * width//2, activation='relu')(decoded)
+
+z = Dense(height * width, activation='sigmoid')(decoded)
+model = Model(input_img, z)
+
+model.compile(optimizer='adadelta', loss='mse') # reporting the accuracy
+
+model.fit(X_train, X_train,
+      epochs=10,
+      batch_size=128,
+      shuffle=True,
+      validation_data=(X_test, X_test))
+
+mid = Model(input_img, y)
+reduced_representation =mid.predict(X_test)
+
+out = Dense(num_classes, activation='softmax')(y)
+reduced = Model(input_img, out)
+reduced.compile(loss='categorical_crossentropy',
+          optimizer='adam', 
+          metrics=['accuracy']) 
+
+reduced.fit(X_train, Y_train,
+      epochs=10,
+      batch_size=128,
+      shuffle=True,
+      validation_data=(X_test, Y_test))
+
+scores = reduced.evaluate(X_test, Y_test, verbose=1) 
+print("Accuracy: ", scores[1])
+ 
+ 
